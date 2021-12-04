7e189c1140ab4ac965a3a161afcd84e0546021b5
ROSARIO BUMATAY <94593410+RosaBumatay@users.noreply.github.com>
2021-12-04 10:32:37 +0800

    20211204

---
+++ Menus/PopUpMenu/app/src/main/java/com/example/popup_menu/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.popup_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.btnShow);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(MainActivity.this, v);
+                popup.setOnMenuItemClickListener(MainActivity.this);
+                popup.inflate(R.menu.popup_menu);
+                popup.show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        Toast.makeText(this, "Selected Item: "+item.getTitle(),Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.search_item:
+                Toast.makeText(this, "Search" + item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.upload_item:
+                Toast.makeText(this, "Upload" + item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.copy_item:
+                Toast.makeText(this, "Copy" + item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.print_item:
+                Toast.makeText(this, "Print" + item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.share_item:
+                Toast.makeText(this, "Share" + item.getTitle(), Toast.LENGTH_SHORT).show();
+
+                return true;
+            case R.id.bookmark_item:
+                Toast.makeText(this, "Marked" + item.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
