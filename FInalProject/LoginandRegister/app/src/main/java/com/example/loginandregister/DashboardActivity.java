ba4561968f5beee525e0b9bcea453bc82bef57d9
MarkFlorida08 <94597596+MarkFlorida08@users.noreply.github.com>
2022-01-14 11:10:42 +0800

    011422

---
+++ FInalProject/LoginandRegister/app/src/main/java/com/example/loginandregister/DashboardActivity.java
@@ -0,0 +1,52 @@
+package com.example.loginandregister;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+    Button LogOut;
+    TextView EmailShow;
+    String EmailHolder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        LogOut = (Button)findViewById(R.id.button);
+        EmailShow = (TextView)findViewById(R.id.EmailShow);
+
+
+        Intent intent = getIntent();
+        EmailHolder = intent.getStringExtra(UserLoginActivity.UserEmail);
+        EmailShow.setText(EmailHolder);
+
+LogOut.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+
+        finish();
+
+        Intent intent = new Intent(DashboardActivity.this, UserLoginActivity.class);
+
+        startActivity(intent);
+
+        Toast.makeText(DashboardActivity.this, "Log Out Successfully", Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+    }
+});
+
+    }
+}
